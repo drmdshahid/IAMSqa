79c2bdb09cddfa5117763605d28512255e5330ea
drmdshahid <drmdshahid@users.noreply.github.com>
2016-02-02 11:00:56 +0530

    Create IAMSgen.meta.js
    for auto-update via greasemonkey

---
+++ IAMSgen.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name       IAMS gen iframe
+// @description    collect q&a in iframe pure
+// @namespace  shahid_IAMS
+// @version    3.2.0b
+// @copyright  2013+, Shahid
+// @include    http://www.iamsonline.in/results.aspx?q=*&qi=*
+// @include    http://iamsonline.in/results.aspx?q=*&qi=*
+// @grant      none
+// @downloadURL https://github.com/drmdshahid/IAMSqa/IAMSgen.js
+// @updateURL   https://github.com/drmdshahid/IAMSqa/IAMSgen.meta.js
+// ==/UserScript==
