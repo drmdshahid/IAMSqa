8a18d258b45d2b81b16e305e279477a6b81190af
drmdshahid <drmdshahid@users.noreply.github.com>
2016-02-02 13:44:17 +0530

    Update and rename IAMS PGI attempt.meta.js to IAMS-PGI-attempt.meta.js

--- downloads/IAMS-PGI-attempt.meta.js
+++ downloads/IAMS-PGI-attempt.meta.js
@@ -6,6 +6,6 @@
 // @version     1.0b
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
 // @grant       none
-// @downloadURL https://github.com/drmdshahid/IAMSqa/IAMS PGI attempt.js
-// @updateURL   https://github.com/drmdshahid/IAMSqa/IAMS PGI attempt.meta.js
+// @downloadURL https://drmdshahid.github.io/IAMSqa/downloads/IAMS-PGI-attempt.js
+// @updateURL   https://drmdshahid.github.io/IAMSqa/downloads/IAMS-PGI-attempt.meta.js
 // ==/UserScript==
