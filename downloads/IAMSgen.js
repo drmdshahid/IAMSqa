727c62aa14fe7cf329f35653f7e08a2da2231550
drmdshahid <drmdshahid@users.noreply.github.com>
2016-02-02 13:38:51 +0530

    Update IAMSgen.js

--- downloads/IAMSgen.js
+++ downloads/IAMSgen.js
@@ -7,8 +7,8 @@
 // @include    http://www.iamsonline.in/results.aspx?q=*&qi=*
 // @include    http://iamsonline.in/results.aspx?q=*&qi=*
 // @grant      none
-// @downloadURL https://github.com/drmdshahid/IAMSqa/IAMSgen.js
-// @updateURL   https://github.com/drmdshahid/IAMSqa/IAMSgen.meta.js
+// @downloadURL http://drmdshahid.github.io/IAMSqa/downloads/IAMSgen.js
+// @updateURL   http://drmdshahid.github.io/IAMSqa/downloads/IAMSgen.meta.js
 // ==/UserScript==
 
 
