8b44d0e3eec0c5406bf4cb53146cf2c6881aecb3
drmdshahid <drmdshahid@users.noreply.github.com>
2016-02-02 11:04:19 +0530

    now on github

--- IAMS PGI attempt.js
+++ IAMS PGI attempt.js
@@ -6,6 +6,8 @@
 // @version     1.0b
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
 // @grant       none
+// @downloadURL https://github.com/drmdshahid/IAMSqa/IAMS PGI attempt.js
+// @updateURL   https://github.com/drmdshahid/IAMSqa/IAMS PGI attempt.meta.js
 // ==/UserScript==
 
 
